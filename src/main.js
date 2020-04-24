36f9cb9422ea2ee09430fec4e4a0071f137c5065
Matt Stroud <lixingke06@gmail.com>
2020-04-24 16:37:40 -0700

    add input validation for amount

--- src/main.js
+++ src/main.js
@@ -3,33 +3,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 import { CurrencyExchange, convertCurrency, storeCurrencyData } from './../src/exchange.js';
-import { displayCurrencyCodes, displayResults } from './../src/userinterface.js';
+import { loadCurrencyCodes, displayResults } from './../src/userinterface.js';
 
 $(document).ready(function () {
-  $("#currency-codes").append(displayCurrencyCodes());
+
+  loadCurrencyCodes();
   $("#error").hide();
 
   $("#exchange").click(function () {
+
     const currencyFrom = $("#currencyFrom").val();
     const currencyTo = $("#currencyTo").val();
     const currencyAmount = parseInt($("#currencyAmount").val());
     sessionStorage.setItem("currencyFrom", " ");
     let result;
 
-    if (sessionStorage.length === 0 || sessionStorage.getItem("currencyFrom") !== currencyFrom) {
-      (async () => {
-        sessionStorage.setItem("currencyFrom", currencyFrom);
-        let currencyExchange = new CurrencyExchange();
-        const response = await currencyExchange.getExchangeRates(currencyFrom);
-        storeCurrencyData(response);
+    if (!currencyAmount) {
+      $("#error").html("Please input a valid number");
+      $("#results").hide();
+      $("#error").slideDown();
+    }
+    else {
+      if (sessionStorage.length === 0 || sessionStorage.getItem("currencyFrom") !== currencyFrom) {
+
+        (async () => {
+
+          sessionStorage.setItem("currencyFrom", currencyFrom);
+          let currencyExchange = new CurrencyExchange();
+          const response = await currencyExchange.getExchangeRates(currencyFrom);
+          storeCurrencyData(response);
+          result = convertCurrency(currencyAmount, currencyTo, sessionStorage);
+          displayResults(result, currencyAmount, currencyFrom, currencyTo);
+        })();
+      } else {
+
         result = convertCurrency(currencyAmount, currencyTo, sessionStorage);
         displayResults(result, currencyAmount, currencyFrom, currencyTo);
-      })();
-    } else {
-      result = convertCurrency(currencyAmount, currencyTo, sessionStorage);
-      displayResults(result, currencyAmount, currencyFrom, currencyTo);
+      }
     }
-
-    console.log(sessionStorage["currencyFrom"]);
   });
 });
