fffa659719c278dcf2172edc90717391ea61f176
Matt Stroud <lixingke06@gmail.com>
2020-04-24 09:38:42 -0700

    work on api logic

---
+++ src/exchange.js
@@ -0,0 +1,15 @@
+export class CurrencyExchange {
+  async getExchangeRates(currency) {
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/${currency}`);
+      let jsonResponse;
+      if (response.ok && response.status == 200) {
+        jsonResponse = await response.json();
+      } else {
+        jsonResponse = false;
+      }
+    } catch (error) {
+      //alert(error.message);
+    }
+  }
+}
